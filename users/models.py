cf1cdc3acb4ce432cf764175d70f5c19e1386b09
kmurata798 <kmurata798@gmail.com>
2020-06-22 22:42:28 -0700

    [Add] Post CRUD functional. Need to fix USER LOGIN/REGISTER

--- users/models.py
+++ users/models.py
@@ -9,8 +9,8 @@ class profile(models.Model):
     def __str__(self):
         return f'{self.user.username}\'s Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         
         img = Image.open(self.image.path)
         
