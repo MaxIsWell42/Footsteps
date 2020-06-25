2dad9d323e7b2c8755072bc63ed618c99b4d35a4
kmurata798 <kmurata798@gmail.com>
2020-06-25 12:29:57 -0700

    settings.py -> changed whitenoise.django.gzipManifest... to whitenoise.storage.Compressed...

--- Footsteps/settings.py
+++ Footsteps/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
