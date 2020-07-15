c463389d9d8b542470fed4bbdce7a9c28ab6840a
kmurata798 <kmurata798@gmail.com>
2020-07-15 11:45:36 -0700

    [Deployment] ran -> heroku run python3 manage.py migrate

--- Footsteps/settings.py
+++ Footsteps/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'q4u67su0^=_7l@0ccihb9=s61nl32vm04qjl*avfu$y!4qns)t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
